5769cf287a1f7236b09c39af5ad5175d51ff809a
Royal Askarov <royalaskar42@gmail.com>
2025-12-25 13:47:24 +0400

    cm2

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+It includes validation for size and a method to calculate the area.
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The length of a side of the square.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
