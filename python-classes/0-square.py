59844156fbde06f4602312070ae5b4762cb60be8
Royal Askarov <royalaskar42@gmail.com>
2025-12-25 13:22:38 +0400

    cm0.2

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+The class is used to represent geometric squares with a private size attribute.
+"""
 class Square:
     def __init__(self, size):
         self.__size = size
