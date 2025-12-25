fe1df87450adfd74f4358bf7d6b351b7ca1f22d0
Royal Askarov <royalaskar42@gmail.com>
2025-12-25 14:37:42 +0400

    cm3

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+It implements properties for the private size attribute and an area method.
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The length of a side of the square.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    # @property
+    def size(self):
+        return self.__size
+
+    # @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
